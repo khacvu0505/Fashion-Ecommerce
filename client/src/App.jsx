cd8e73e23ba9b62acfb89037f78026403996d89d
khacvu0505 <khacvu0505@gmail.com>
2022-01-21 11:56:59 +0700

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,15 +6,30 @@ import Register from "./page/Register";
 import Login from "./page/Login";
 import Cart from "./page/Cart";
 
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 const App = () => {
+  const user = true;
   return (
     <div>
-      {/* <Home /> */}
-      {/* <ProductList /> */}
-      {/* <Product /> */}
-      {/* <Register /> */}
-      {/* <Login /> */}
-      <Cart />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/products" element={<ProductList />} />
+          <Route exact path="/product/:id" element={<Product />} />
+          <Route exact path="/cart" element={<Cart />} />
+          <Route
+            exact
+            path="/login"
+            element={user ? <Navigate to="/" /> : <Login />}
+          />
+          <Route
+            exact
+            path="/register"
+            element={user ? <Navigate to="/" /> : <Register />}
+          />
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
